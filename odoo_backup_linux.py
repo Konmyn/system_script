43fb2dedcaa66dc68d5385ae0ba03a829f2bef5a
Ethan <konmyn@163.com>
2016-12-06 14:25:08 +0800

    init test

--- odoo_backup_linux.py
+++ odoo_backup_linux.py
@@ -1,47 +1,44 @@
-# -*- coding: utf8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 
 import os, datetime
 
 # postgreSQL的当前用户
-pg_user = 'administrator'
+PG_USER = 'root'
 # 当前用户(administrator)的postgres登录密码
-pg_pwd = 'admin'
+PG_PASSWORD = 'admin'
 # 需要备份的数据库列表
-db_names = ['chc','test']
+DATABASES = ['kalisign']
 # 备份文件夹
-backup_dir = 'D:\\pgbackup\\'
+backup_dir = '/root/backup/'
 # data_dir in odoo.conf and then specified for filestore.
-data_dir = 'D:\\ERP\\data\\filestore\\'
-odoo_dir = 'D:\\ERP\\'
+data_dir = '/root/.local/share/Odoo'
 # log文档的全路径
 log_path = backup_dir + 'backup.log'
-# postgres安装的执行文件路径，最好加入系统path当中
-# pg_path = 'C:\\Program Files\\PostgreSQL\\9.6\\bin\\'
+
 
 # time related variables
 # 获取当前时间
 _now = datetime.datetime.now()
 # 2016-11-02-11-28-21, 用类似这样的格式作为文件前缀
-today_file = _now.strftime("%Y-%m-%d-%H-%M-%S")
+today_file = _now.strftime("%Y%m%d")
+print today_file
 # 2016-11-02 14:47:31, 用类似这样的时间格式作为log信息记录
 today_str = _now.strftime("%Y-%m-%d %H:%M:%S")
+print today_str
 
 # 创建日志函数
 def writeLogs(filename, contents):
-    f = file(filename,'a+')
-    f.write(contents)
-    f.close()
-    return True
+    log_file = file(filename,'a+')
+    log_file.write(contents)
+    log_file.close()
 
 # 数据库备份函数
-def pgdbBackup(user, pwd, db_list, file_path, file_prefix):
-    # 把当前postgreSQL用户的密码写入环境变量
-    # os.putenv('PGPASSWORD', pwd)
-    os.environ['PGPASSWORD'] = pwd
-    for datebase in db_list:
-        backup_name = file_prefix + '_' + datebase + '.backup'
-        backup_cmd = "pg_dump -U {} -w -f {}{} {}".format(user, file_path, backup_name, datebase)
+def postgress_database_backup(user, databases, file_path, file_prefix):
+    for datebase in databases:
+        backup_name = '{}.{}.backup.gz'.format(file_prefix, datebase)
+        backup_cmd = "pg_dump {} | gzip > {}".format(datebase, file_path+backup_name)
         if os.system(backup_cmd):
             writeLogs(log_path, "Backup database failed!\n")
         else:
@@ -49,10 +46,9 @@ def pgdbBackup(user, pwd, db_list, file_path, file_prefix):
     return True
 
 # 文件夹备份函数
-def fileBackup(directory, file_path, file_prefix):
-    backup_name = file_prefix + '_filestore.zip'
-    # make sure you install 7-zip in windows and add it into system path.
-    zip_cmd = "7z a {}{} -r {}".format(file_path, backup_name, directory)
+def file_dir_backup(directory, file_path, file_prefix):
+    backup_name = '{}.data.backup.gz'.format(file_prefix)
+    cmp_cmd = "gzip {} > {}".format(file_path, backup_name)
     if os.system(zip_cmd):
         writeLogs(log_path, "Backup filestore failed!\n")
         return False
@@ -87,9 +83,9 @@ def main():
     target software: odoo 10 enterprise
     '''
     writeLogs(log_path, "-"*79 + "\nOperation time: {}\n".format(today_str))
-    pgdbBackup(pg_user, pg_pwd, db_names, backup_dir, today_file)
-    fileBackup(data_dir, backup_dir, today_file)
-    deleteBackup(backup_dir, 10)
+    postgress_database_backup(PG_USER, pg_pwd, DATABASES, backup_dir, today_file)
+    # file_dir_backup(data_dir, backup_dir, today_file)
+    # deleteBackup(backup_dir, 10)
 
 if __name__ == "__main__":
     main()
