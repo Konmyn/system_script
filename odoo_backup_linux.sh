82f4361e72186b1846d52ffbfd916c96d5d988a9
Ethan <konmyn@163.com>
2016-12-07 10:35:11 +0800

    add someone's db_tools for odoo, stop self making.

--- odoo_backup_linux.sh
+++ odoo_backup_linux.sh
@@ -2,7 +2,7 @@
 
 # crontab -e
 # crontab -l
-
+# not work in crontab(aliyun, user:root)
 pg_dump kalisign | gzip > /root/backup/$(date +%Y-%m-%d).kalisign.backup.gz
 tar czf /root/backup/$(date +%Y-%m-%d).data.tar.gz /root/.local/share/Odoo/filestore
 rm -f /root/backup/$(date -d'-7 day' +%Y-%m-%d)*
